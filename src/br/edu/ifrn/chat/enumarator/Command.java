2ad080152532ad894380468ca381705c9fe4f8b6
joaorobertoifrn <joao.roberto@academico.ifrn.edu.br>
2017-05-25 23:05:56 -0300

    Inicio do projeto

---
+++ src/br/edu/ifrn/chat/enumarator/Command.java
@@ -0,0 +1,6 @@
+package br.edu.ifrn.chat.enumarator;
+
+public enum Command {
+    
+    CONNECT, CONNECTED, DISCONNECT, DISCONNECTED, NOT_CONNECTED, RECEIVED, SEND, SEND_RESERVED, SEND_ALL, USERS_ONLINE
+}
---
+++ src/br/edu/ifrn/chat/service/ClientService.java
@@ -0,0 +1,34 @@
+package br.edu.ifrn.chat.service;
+
+import br.edu.ifrn.chat.model.DataPackage;
+import java.io.*;
+import java.net.*;
+
+public class ClientService {
+    
+    //Insira seu IP, caso execute em uma máquina diferente do servidor... 
+    //private static final String IP_CLIENT = "localhost";
+    //private static final int PORT = 7896;
+    
+    private Socket socket;
+    private ObjectOutputStream output;
+    
+    public Socket connect(String IP_CLIENT, int PORT) {
+        try {
+            this.socket = new Socket(IP_CLIENT, PORT);
+            this.output = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return socket;
+    }
+    
+    public void send(DataPackage dataPackage) {
+        try {
+            output.writeObject(dataPackage);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
